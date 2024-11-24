8af3a5a90f11e46d604f13ad2a3003506ea27736
Burak Serdar <bserdar@computer.org>
2024-11-24 12:10:56 -0700

    Explain space handling

--- src/chp2/regex-submatch/main.go
+++ src/chp2/regex-submatch/main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
+	// This regular expression allows extra spaces before and after the
+	// property name and value using \s*
 	re := regexp.MustCompile(`^\s*(\w+)\s*=\s*(\w+)\s*$`)
 	result := re.FindStringSubmatch(`  property = 12 `)
 	fmt.Printf("Key: %s value: %s\n", result[1], result[2])
