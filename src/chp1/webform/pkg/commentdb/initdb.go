488505f57f721d2a452fae4fca9f425b3f264dee
Burak Serdar <bserdar@computer.org>
2024-11-27 16:33:50 -0700

    Make it consistent with text

--- src/chp1/webform/pkg/commentdb/initdb.go
+++ src/chp1/webform/pkg/commentdb/initdb.go
@@ -5,10 +5,12 @@ import (
 	"database/sql"
 )
 
+const createStmt = `create table if not exists comments (
+email TEXT,
+comment TEXT)`
+
 func InitDB(conn *sql.DB) {
-	_, err := conn.ExecContext(context.Background(), `create table if not exists comments (
-  email TEXT,
-  comment TEXT)`)
+	_, err := conn.ExecContext(context.Background(), createStmt)
 	if err != nil {
 		panic(err)
 	}
