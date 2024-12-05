3d080ffc66029d5a2494348ce50e887c3585ad13
Burak Serdar <bserdar@computer.org>
2024-12-05 11:52:53 -0700

    Add timeout

--- src/chp10/signal/main.go
+++ src/chp10/signal/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -22,7 +23,10 @@ func main() {
 	signal.Notify(sigTerm, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-sigTerm
-		server.Shutdown(context.Background())
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.
+			Second)
+		defer cancel()
+		server.Shutdown(ctx)
 	}()
 
 	server.ListenAndServe()
